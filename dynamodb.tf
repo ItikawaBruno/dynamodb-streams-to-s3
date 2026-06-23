bf5e0623be21fa13d40792069d5b31eafe380809
ItikawaBruno <bruno.itikawa@gmail.com>
2026-06-23 07:37:33 -0300

    deploy

---
+++ dynamodb.tf
@@ -0,0 +1,14 @@
+resource "aws_dynamodb_table" "usuarios" {
+    name = "usuarios"
+    billing_mode = "PAY_PER_REQUEST"
+
+    hash_key = "id"
+
+    attribute {
+        name = "id"
+        type = "S"
+    }
+
+    stream_enabled = true
+    stream_view_type = "NEW_IMAGE"
+}
